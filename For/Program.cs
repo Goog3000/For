288b2edd06f17b14b2aa7daef29840a2b478c61b
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-20 12:04:46 +0100

    Add project files.

---
+++ For/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first value :");
+            int firstValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter limit :");
+            int limit = int.Parse(Console.ReadLine());
+
+            for (int i = firstValue; i < limit; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
