f869d815b46079e0706ea5aa716ebb29cad2d153
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-23 12:23:17 +0100

    i+j(for)

---
+++ ForIJ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForIJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine("i: " + i);
+            }
+            for (int j = 0; j <= 10; j++)
+            {
+                Console.WriteLine("j: " + j);
+            }
+        }
+    }
+}
