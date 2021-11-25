c07cf66c9f26232f457c8a94426e71e7ffb0264c
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-25 11:49:05 +0100

    primeNumber

--- ForIJ/Program.cs
+++ ForIJ/Program.cs
@@ -4,16 +4,35 @@ namespace ForIJ
 {
     class Program
     {
+        static bool primeNumber(int value)
+        {
+            for (int i = 2; i < value; i++)
+            {
+                if (value % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
-            for (int i = 10; i >= 0; i--)
+            Console.WriteLine("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[number];
+
+            for (int i = 2; numbers[number -1]==0; i++)
             {
-                Console.WriteLine("i: " + i);
+                if (primeNumber(i))
+                {
+                    int index = Array.IndexOf(numbers, 0);
+                    if (index!= -1)
+                    {
+                        numbers[index] = i;
+                    }
+                }                
             }
-            for (int j = 0; j <= 10; j++)
-            {
-                Console.WriteLine("j: " + j);
-            }
+            Console.WriteLine(string.Join(",", numbers));
         }
     }
 }
